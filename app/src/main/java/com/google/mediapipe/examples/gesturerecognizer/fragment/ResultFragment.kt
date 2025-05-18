1a2c2815a2bbb043c774f4886a20e2aed8aa71e8
Sidou104 <sidoubrahami10@gmail.com>
2025-05-19 00:51:31 +0100

    modification sur le quiz

--- app/src/main/java/com/google/mediapipe/examples/gesturerecognizer/fragment/ResultFragment.kt
+++ app/src/main/java/com/google/mediapipe/examples/gesturerecognizer/fragment/ResultFragment.kt
@@ -22,6 +22,7 @@ class ResultFragment : Fragment() {
             return fragment
         }
     }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
